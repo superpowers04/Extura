6ff9f2e3d164689f7aaeb3920caeb625b72d42f8
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2024-01-20 19:46:57 -0600

    Merge pull request #147 from KitCat962/disable-vanilla-rotations
    Fixed vanilla inventory behaving odd due to previous pr

--- common/src/main/java/org/figuramc/figura/mixin/gui/InventoryScreenMixin.java
+++ common/src/main/java/org/figuramc/figura/mixin/gui/InventoryScreenMixin.java
@@ -7,7 +7,6 @@ import org.figuramc.figura.avatar.AvatarManager;
 import org.figuramc.figura.config.Configs;
 import org.figuramc.figura.model.rendering.EntityRenderMode;
 import org.figuramc.figura.utils.ui.UIHelper;
-import org.joml.Vector3f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -21,7 +20,7 @@ public class InventoryScreenMixin {
         if (!Configs.FIGURA_INVENTORY.value || AvatarManager.panic)
             return;
 
-        UIHelper.drawEntity(x, y, size, mouseX, mouseY, entity, guiGraphics, new Vector3f(), EntityRenderMode.MINECRAFT_GUI);
+        UIHelper.drawEntity(x, y, size, (float) Math.atan(mouseY / 40f) * 20f, (float) -Math.atan(mouseX / 40f) * 20f, entity, guiGraphics, EntityRenderMode.MINECRAFT_GUI);
         ci.cancel();
     }
 }
