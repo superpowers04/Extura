adfb9fe9f786a677c3a01feb3786c40d03aa42f8
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2024-02-04 05:15:41 -0600

    add certificates

--- common/src/main/java/org/figuramc/figura/utils/PlatformUtils.java
+++ common/src/main/java/org/figuramc/figura/utils/PlatformUtils.java
@@ -4,6 +4,9 @@ import dev.architectury.injectables.annotations.ExpectPlatform;
 import net.minecraft.SharedConstants;
 import org.figuramc.figura.FiguraMod;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.nio.file.Path;
 
 public class PlatformUtils {
@@ -50,5 +53,20 @@ public class PlatformUtils {
                 return 1;
         }
         return 0;
+    }
+
+    public enum ModLoader {
+        FORGE,
+        FABRIC
+    }
+
+    @ExpectPlatform
+    public static ModLoader getModLoader(){
+        throw new AssertionError();
+    }
+
+    @ExpectPlatform
+    public static InputStream loadFileFromRoot(String file) throws FileNotFoundException {
+        throw new AssertionError();
     }
 }
