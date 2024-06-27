24d820b2dfda5836897d018b0e78dad9e2f593a7
soup587 <jr587atroblox@gmail.com>
2024-06-27 19:18:18 +0100

    half broken damage event

--- common/src/main/java/org/figuramc/figura/mixin/ClientPacketListenerMixin.java
+++ common/src/main/java/org/figuramc/figura/mixin/ClientPacketListenerMixin.java
@@ -1,10 +1,18 @@
 package org.figuramc.figura.mixin;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.protocol.game.ClientboundDamageEventPacket;
+import net.minecraft.world.level.Level;
 import org.figuramc.figura.FiguraMod;
+import org.figuramc.figura.avatar.Avatar;
+import org.figuramc.figura.avatar.AvatarManager;
+import org.figuramc.figura.lua.api.entity.EntityAPI;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = ClientPacketListener.class, priority = 999)
@@ -14,5 +22,17 @@ public class ClientPacketListenerMixin {
     private void sendUnsignedCommand(String command, CallbackInfoReturnable<Boolean> cir) {
         if (command.startsWith(FiguraMod.MOD_ID))
             cir.setReturnValue(false);
+    }
+
+    @Inject(at = @At("HEAD"), method = "handleDamageEvent")
+    private void handleDamageEvent(ClientboundDamageEventPacket packet, CallbackInfo ci) {
+        Avatar avatar = AvatarManager.getAvatarForPlayer(FiguraMod.getLocalPlayerUUID());
+        Level level = Minecraft.getInstance().player.level();
+        if (avatar != null) ;
+            avatar.damageEvent(
+            level.registryAccess().registry(Registries.DAMAGE_TYPE).get().toString(),
+            EntityAPI.wrap(level.getEntity(packet.sourceCauseId())),
+            EntityAPI.wrap(level.getEntity(packet.sourceDirectId()))
+            );
     }
 }
