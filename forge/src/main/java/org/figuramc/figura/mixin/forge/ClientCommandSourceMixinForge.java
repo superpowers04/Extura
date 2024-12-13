27885bc6664bdae67701be29a9b10a37a1bc8aae
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2024-12-12 19:41:13 -0600

    fix commands definitely on forge forever and always

---
+++ forge/src/main/java/org/figuramc/figura/mixin/forge/ClientCommandSourceMixinForge.java
@@ -0,0 +1,40 @@
+package org.figuramc.figura.mixin.forge;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.network.chat.Component;
+import org.figuramc.figura.utils.FiguraClientCommandSource;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(CommandSourceStack.class)
+abstract class CommandSourceMixinForge implements FiguraClientCommandSource {
+
+    @Override
+    public void figura$sendFeedback(Component message) {
+        Minecraft.getInstance().gui.getChat().addMessage(message);
+        Minecraft.getInstance().getNarrator().sayNow(message);
+    }
+
+    @Override
+    public void figura$sendError(Component message) {
+        figura$sendFeedback(Component.literal("").append(message).withStyle(ChatFormatting.RED));
+    }
+
+    @Override
+    public Minecraft figura$getClient() {
+        return Minecraft.getInstance();
+    }
+
+    @Override
+    public LocalPlayer figura$getPlayer() {
+        return Minecraft.getInstance().player;
+    }
+
+    @Override
+    public ClientLevel figura$getWorld() {
+        return Minecraft.getInstance().level;
+    }
+}
