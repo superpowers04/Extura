934490c1287860fce8b1d319629ecc255c69dfa7
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2025-02-24 15:37:38 -0600

    Merge pull request #260 from soup587/xp
    implement get xp for next level

--- common/src/main/java/org/figuramc/figura/lua/api/entity/PlayerAPI.java
+++ common/src/main/java/org/figuramc/figura/lua/api/entity/PlayerAPI.java
@@ -79,6 +79,13 @@ public class PlayerAPI extends LivingEntityAPI<Player> {
     }
 
     @LuaWhitelist
+    @LuaMethodDoc("player.get_xp_for_next_level")
+    public int getExperienceForNextLevel(){
+        checkEntity();
+        return entity.getXpNeededForNextLevel();
+    }
+
+    @LuaWhitelist
     @LuaMethodDoc("player.get_experience_level")
     public int getExperienceLevel() {
         checkEntity();
