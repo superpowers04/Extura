da1423ee898a806db9083880e7b727f9623cb8ea
Super <38338700+superpowers04@users.noreply.github.com>
2025-02-28 03:39:55 -0500

    Forge is shaking and crying because i didn't make fake classes

---
+++ forge/src/main/java/me/superpowers04/extura/forge/ExturaInsecureModStub.java
@@ -0,0 +1,9 @@
+package me.superpowers04.extura.forge;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("extura_insecure")
+public class ExturaInsecureModStub {
+    public ExturaInsecureModStub() {}
+}
+
