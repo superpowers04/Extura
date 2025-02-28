637d31da9b734094013008e918a9011433f8ce69
Super <38338700+superpowers04@users.noreply.github.com>
2025-02-28 06:17:19 -0500

    component.literal instead of figura.extura lmoa

--- common/src/main/java/org/figuramc/figura/gui/screens/WardrobeScreen.java
+++ common/src/main/java/org/figuramc/figura/gui/screens/WardrobeScreen.java
@@ -122,7 +122,7 @@ public class WardrobeScreen extends AbstractPanelScreen {
 		final String buildString = FiguraMod.VERSION.noBuildString();
 		final String buildVersion = buildString.substring(0,buildString.lastIndexOf('-'));
 		final String dateBuilt = new Date(new Long(FiguraMod.VERSION.noBuildString().substring(buildVersion.length()+1))).toString();
-		MutableComponent versionText = FiguraText.of("Extura").append(", Compatible with " + buildVersion + "\n Built on " + dateBuilt).withStyle(ChatFormatting.ITALIC);
+		MutableComponent versionText = Component.literal("Extura").append(", Compatible with " + buildVersion + "\n Built on " + dateBuilt).withStyle(ChatFormatting.ITALIC);
 		final int versionStatus = NetworkStuff.latestVersion != null ? NetworkStuff.latestVersion.compareTo(FiguraMod.VERSION) : 0;
 		boolean oldVersion = versionStatus > 0;
 		if (oldVersion) {
