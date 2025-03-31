7fcd71b111a9a2610fd32a8cd313de437836756c
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2025-03-30 22:45:57 -0400

    feat: record collection data in NBT

--- common/src/main/java/org/figuramc/figura/parsers/BlockbenchModelParser.java
+++ common/src/main/java/org/figuramc/figura/parsers/BlockbenchModelParser.java
@@ -1,5 +1,8 @@
 package org.figuramc.figura.parsers;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multimaps;
 import com.google.gson.*;
 import net.minecraft.nbt.*;
 import org.figuramc.figura.FiguraMod;
@@ -28,6 +31,7 @@ public class BlockbenchModelParser {
     private int animationOffset = 0;
 
     //used during the parser
+    private final Multimap<String, Integer> collectionMap = HashMultimap.create();
     private final HashMap<String, CompoundTag> elementMap = new HashMap<>();
     private final HashMap<String, ListTag> animationMap = new HashMap<>();
     private final HashMap<String, TextureData> textureMap = new HashMap<>();
@@ -59,6 +63,16 @@ public class BlockbenchModelParser {
         //emissive textures are not put into the texture map, so we need to fix parts texture ids
         parseTextures(avatarFolder, sourceFile, folders, modelName, textures, model.textures, model.resolution);
 
+        //parse collection
+        ListTag cns = new ListTag();
+        if (model.collections != null)
+            for (int i = 0; i < model.collections.length; i++) {
+                var c = model.collections[i];
+                cns.add(StringTag.valueOf(c.name));
+                for (String u: c.children) collectionMap.put(u, i);
+            }
+        nbt.put("cn", cns);
+
         //parse elements into a map of UUID (String) -> NbtCompound (the element)
         //later when parsing the outliner, we fetch the elements from this map
         parseElements(model.elements);
@@ -72,6 +86,7 @@ public class BlockbenchModelParser {
         nbt.put("chld", parseOutliner(model.outliner, true));
 
         //clear variables used by the parser
+        collectionMap.clear();
         elementMap.clear();
         animationMap.clear();
         textureMap.clear();
@@ -177,9 +192,7 @@ public class BlockbenchModelParser {
                     FiguraMod.LOGGER.error("", e);
 
                 //otherwise, load from the source stored in the model
-                String sourceTex = texture.source;
-                if(sourceTex.startsWith("data:image/png;base64,"))sourceTex = sourceTex.substring(22);
-                source = Base64.getDecoder().decode(sourceTex);
+                source = Base64.getDecoder().decode(texture.source.substring("data:image/png;base64,".length()));
                 path = folders + modelName + "." + name;
                 FiguraMod.debug("Loaded {} Texture \"{}\" from {}", textureType.toUpperCase(Locale.US), name, path);
             }
@@ -278,6 +291,13 @@ public class BlockbenchModelParser {
                 nbt.put("mesh_data", data);
             }
 
+            //find collections
+            var prs = new ArrayList<>(collectionMap.get(element.uuid));
+            if (!prs.isEmpty()) {
+                byte pr[] = new byte[prs.size()];
+                for (int i = 0; i < prs.size(); i++) pr[i] = (byte) (int) prs.get(i);
+                nbt.put("pr", new ByteArrayTag(pr));
+            }
 
             elementMap.put(id, nbt);
         }
@@ -662,6 +682,14 @@ public class BlockbenchModelParser {
 
             //parent type
             parseParent(group.name, groupNbt);
+
+            //find collections
+            var prs = new ArrayList<>(collectionMap.get(group.uuid));
+            if (!prs.isEmpty()) {
+                byte pr[] = new byte[prs.size()];
+                for (int i = 0; i < prs.size(); i++) pr[i] = (byte) (int) prs.get(i);
+                groupNbt.put("pr", new ByteArrayTag(pr));
+            }
 
             //parse children
             if (!(group.children == null || group.children.isEmpty()))
