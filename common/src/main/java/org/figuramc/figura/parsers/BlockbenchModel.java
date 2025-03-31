de15a0b2622193da82b51afac15df3b293f3cdfc
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2025-03-30 22:45:57 -0400

    feat: record collection data in NBT

--- common/src/main/java/org/figuramc/figura/parsers/BlockbenchModel.java
+++ common/src/main/java/org/figuramc/figura/parsers/BlockbenchModel.java
@@ -2,6 +2,7 @@ package org.figuramc.figura.parsers;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ public class BlockbenchModel {
     Texture[] textures;
     Element[] elements;
     Animation[] animations;
+    BCollection @Nullable[] collections;
 
     // do not reflection-parse the outliner
     // as it can be either an object or a string
@@ -121,5 +123,11 @@ public class BlockbenchModel {
 
     public static class KeyFrameData {
         String x, y, z;
+    }
+
+    public static class BCollection {
+        String uuid;
+        String name;
+        String[] children;
     }
 }
