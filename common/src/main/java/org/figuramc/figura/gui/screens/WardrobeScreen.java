b44159bb4c623682dd3c9888b064393969a715bd
Super <38338700+superpowers04@users.noreply.github.com>
2025-05-04 16:37:26 -0400

    The buildtime isn't worth the bugs sobbing emoji

--- common/src/main/java/org/figuramc/figura/gui/screens/WardrobeScreen.java
+++ common/src/main/java/org/figuramc/figura/gui/screens/WardrobeScreen.java
@@ -117,10 +117,8 @@ public class WardrobeScreen extends AbstractPanelScreen {
 		// -- bottom -- // 
 
 		// version
-		final String buildString = FiguraMod.VERSION.noBuildString();
-		final String buildVersion = buildString.substring(0,buildString.lastIndexOf('-'));
-		final String dateBuilt = new Date(new Long(FiguraMod.VERSION.noBuildString().substring(buildVersion.length()+1))).toString();
-		MutableComponent versionText = Component.literal("Extura").append(", Compatible with " + buildVersion + "\n Built on " + dateBuilt).withStyle(ChatFormatting.ITALIC);
+		final String buildVersion = FiguraMod.VERSION.noBuildString();
+		MutableComponent versionText = Component.literal("Extura").append(", Compatible with " + buildVersion).withStyle(ChatFormatting.ITALIC);
 		final int versionStatus = NetworkStuff.latestVersion != null ? NetworkStuff.latestVersion.compareTo(FiguraMod.VERSION) : 0;
 		boolean oldVersion = versionStatus > 0;
 		if (oldVersion) {
