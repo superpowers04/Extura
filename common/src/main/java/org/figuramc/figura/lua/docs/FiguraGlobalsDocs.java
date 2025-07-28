168ea833c365b41dcb0f3f9833b7aa2b5880f927
Super <38338700+superpowers04@users.noreply.github.com>
2025-07-28 07:58:05 -0400

    Update addScript documentation

--- common/src/main/java/org/figuramc/figura/lua/docs/FiguraGlobalsDocs.java
+++ common/src/main/java/org/figuramc/figura/lua/docs/FiguraGlobalsDocs.java
@@ -120,6 +120,10 @@ public abstract class FiguraGlobalsDocs {
                     @LuaMethodOverload(
                             argumentTypes = {String.class, String.class},
                             argumentNames = {"scriptName", "scriptContents"}
+                    ),
+                    @LuaMethodOverload(
+                            argumentTypes = {String.class, String.class},
+                            argumentNames = {"scriptName", "scriptContents", "side"}
                     )
             },
             value = "globals.add_script"
