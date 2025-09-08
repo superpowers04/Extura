93ff7bb4967699de8cb337adbbe8b897d5d9f4ef
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2025-09-08 13:13:14 -0600

    Merge pull request #373 from Bitslayn/Options

--- common/src/main/java/org/figuramc/figura/lua/docs/FiguraListDocs.java
+++ common/src/main/java/org/figuramc/figura/lua/docs/FiguraListDocs.java
@@ -10,6 +10,7 @@ import net.minecraft.locale.Language;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.player.PlayerModelPart;
 import net.minecraft.world.item.ItemDisplayContext;
@@ -19,6 +20,7 @@ import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.levelgen.Heightmap;
 import org.figuramc.figura.FiguraMod;
 import org.figuramc.figura.animation.Animation;
+import org.figuramc.figura.config.Configs;
 import org.figuramc.figura.mixin.input.KeyMappingAccessor;
 import org.figuramc.figura.ducks.GameEffects;
 import org.figuramc.figura.model.ParentType;
@@ -60,6 +62,7 @@ public class FiguraListDocs {
         for (FiguraTextureSet.OverrideType value : FiguraTextureSet.OverrideType.values())
             add(value.name());
     }};
+    private static final Set<String> KEY_IDS = KeyMappingAccessor.getAll().keySet();
     private static final LinkedHashSet<String> ENTITY_POSES = new LinkedHashSet<>() {{
         for (Pose value : Pose.values())
             add(value.name());
@@ -83,9 +86,8 @@ public class FiguraListDocs {
         for (Animation.LoopMode value : Animation.LoopMode.values())
             add(value.name());
     }};
-    private static final LinkedHashMap<String, List<String>> COLORS = new LinkedHashMap<>() {{
-        for (ColorUtils.Colors value : ColorUtils.Colors.values())
-            put(value.name(), List.of(value.name()));
+    private static final Set<String> COLORS = new LinkedHashSet<>() {{
+        for (ColorUtils.Colors value : ColorUtils.Colors.values()) add(value.name());
     }};
     private static final LinkedHashSet<String> PLAYER_MODEL_PARTS = new LinkedHashSet<>() {{
         for (PlayerModelPart value : PlayerModelPart.values()) {
@@ -117,6 +119,11 @@ public class FiguraListDocs {
         for (ResourceLocation resourceLocation : BuiltInRegistries.REGISTRY.keySet())
             add(resourceLocation.getPath());
     }};
+    private static final Set<String> FIGURA_CONFIGS = Configs.REGISTRY.keySet();
+    private static final LinkedHashSet<String> SOUND_SOURCES = new LinkedHashSet<>() {{
+        for (SoundSource value : SoundSource.values())
+            add(value.name());
+    }};
 
     private enum ListDoc {
         KEYBINDS(() -> FiguraListDocs.KEYBINDS, "Keybinds", "keybinds", 2),
@@ -137,7 +144,9 @@ public class FiguraListDocs {
         BLOCK_RAYCAST_TYPE(() -> FiguraListDocs.BLOCK_RAYCAST_TYPE, "BlockRaycastTypes", "block_raycast_types", 1),
         FLUID_RAYCAST_TYPE(() -> FiguraListDocs.FLUID_RAYCAST_TYPE, "FluidRaycastTypes", "fluid_raycast_types", 1),
         HEIGHTMAP_TYPE(() -> FiguraListDocs.HEIGHTMAP_TYPE, "HeightmapTypes", "heightmap_types", 1),
-        REGISTRIES(() -> FiguraListDocs.REGISTRIES, "Registries", "registries", 1);
+        REGISTRIES(() -> FiguraListDocs.REGISTRIES, "Registries", "registries", 1),
+        FIGURA_CONFIGS(() -> FiguraListDocs.FIGURA_CONFIGS, "FiguraConfigs", "figura_configs", 1),
+        SOUND_SOURCES(() -> FiguraListDocs.SOUND_SOURCES, "SoundSources", "sound_sources", 2);
 
         private final Supplier<Object> supplier;
         private final String name, id;
@@ -152,7 +161,7 @@ public class FiguraListDocs {
 
         private Collection<?> get() {
             Object obj = supplier.get();
-            if (obj instanceof LinkedHashSet<?> set)
+            if (obj instanceof Set<?> set)
                 return set;
             else if (obj instanceof Map<?, ?> map)
                 return map.entrySet();
