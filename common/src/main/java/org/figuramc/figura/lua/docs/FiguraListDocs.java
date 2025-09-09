84156a3a3060410cacbdb5fb8061724bf181cabf
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2025-09-09 14:22:32 -0600

    Merge pull request #254 from penguinencounter/moretextures
    Expanded texture APIs

--- common/src/main/java/org/figuramc/figura/lua/docs/FiguraListDocs.java
+++ common/src/main/java/org/figuramc/figura/lua/docs/FiguraListDocs.java
@@ -25,6 +25,7 @@ import org.figuramc.figura.mixin.input.KeyMappingAccessor;
 import org.figuramc.figura.mixin.render.GameRendererAccessor;
 import org.figuramc.figura.model.ParentType;
 import org.figuramc.figura.model.rendering.EntityRenderMode;
+import org.figuramc.figura.model.rendering.texture.FiguraTexture;
 import org.figuramc.figura.model.rendering.texture.FiguraTextureSet;
 import org.figuramc.figura.model.rendering.texture.RenderTypes;
 import org.figuramc.figura.utils.ColorUtils;
@@ -124,6 +125,14 @@ public class FiguraListDocs {
         for (SoundSource value : SoundSource.values())
             add(value.name());
     }};
+    private static final LinkedHashSet<String> TEXTURE_OVERFLOW_STRATEGIES = new LinkedHashSet<String>() {{
+        for (FiguraTexture.TextureOverflowStrategy strategy : FiguraTexture.TextureOverflowStrategy.values())
+            add(strategy.primaryName);
+    }};
+    private static final LinkedHashSet<String> BLEND_MODES = new LinkedHashSet<String>() {{
+        for (FiguraTexture.BlendMode mode : FiguraTexture.BlendMode.values())
+            add(mode.name);
+    }};
 
     private enum ListDoc {
         KEYBINDS(() -> FiguraListDocs.KEYBINDS, "Keybinds", "keybinds", 2),
@@ -145,6 +154,8 @@ public class FiguraListDocs {
         FLUID_RAYCAST_TYPE(() -> FiguraListDocs.FLUID_RAYCAST_TYPE, "FluidRaycastTypes", "fluid_raycast_types", 1),
         HEIGHTMAP_TYPE(() -> FiguraListDocs.HEIGHTMAP_TYPE, "HeightmapTypes", "heightmap_types", 1),
         REGISTRIES(() -> FiguraListDocs.REGISTRIES, "Registries", "registries", 1),
+        TEXTURE_OVERFLOW_STRATEGIES(() -> FiguraListDocs.TEXTURE_OVERFLOW_STRATEGIES, "TextureOverflowStrategies", "texture_overflow_strategies", 1),
+        BLEND_MODES(() -> FiguraListDocs.BLEND_MODES, "BlendModes", "blend_modes", 1),
         FIGURA_CONFIGS(() -> FiguraListDocs.FIGURA_CONFIGS, "FiguraConfigs", "figura_configs", 1),
         SOUND_SOURCES(() -> FiguraListDocs.SOUND_SOURCES, "SoundSources", "sound_sources", 2);
 
