0503d848a1478f5d404b230800bd70c2f7f62f83
jess <jr587atroblox@gmail.com>
2025-09-13 03:15:55 +0100

    implement forcing first person model rendering

--- common/src/main/java/org/figuramc/figura/lua/api/RendererAPI.java
+++ common/src/main/java/org/figuramc/figura/lua/api/RendererAPI.java
@@ -68,6 +68,8 @@ public class RendererAPI {
     @LuaWhitelist
     @LuaFieldDoc("renderer.render_gui")
     public boolean renderGUI = true;
+    @LuaFieldDoc("renderer.render_first_person")
+    public boolean renderFirstPerson = false;
 
     public FiguraVec3 cameraPos;
     public FiguraVec3 cameraPivot, cameraOffsetPivot;
@@ -181,6 +183,26 @@ public class RendererAPI {
             value = "renderer.set_render_hud")
     public RendererAPI setRenderHUD(boolean renderHUD) {
         this.renderHUD = renderHUD;
+        return this;
+    }
+
+    @LuaWhitelist
+    @LuaMethodDoc("renderer.should_render_first_person")
+    public boolean shouldRenderFirstPerson() {
+        return renderFirstPerson;
+    }
+
+    @LuaWhitelist
+    @LuaMethodDoc(
+            overloads = @LuaMethodOverload(
+                    argumentTypes = Boolean.class,
+                    argumentNames = "bool"
+            ),
+            aliases = "renderFirstPerson",
+            value = "renderer.set_render_first_person"
+    )
+    public RendererAPI setRenderFirstPerson(Boolean bool) {
+        this.renderFirstPerson = bool;
         return this;
     }
 
@@ -791,6 +813,7 @@ public class RendererAPI {
 			case "renderJumpMeter" -> renderJumpMeter;
 			case "renderEffects" -> renderEffects;
 			case "renderGUI" -> renderGUI;
+            case "renderFirstPerson" -> renderFirstPerson;
             default -> null;
         };
     }
@@ -810,6 +833,7 @@ public class RendererAPI {
 			case "renderJumpMeter" -> renderJumpMeter = value;
 			case "renderEffects" -> renderEffects = value;
 			case "renderGUI" -> renderGUI = value;
+            case "renderFirstPerson" -> renderFirstPerson = value;
             default -> throw new LuaError("Cannot assign value on key \"" + key + "\"");
         }
     }
