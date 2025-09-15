dd979a5b7bfd4cbad3f3c558a33e65b9500517c6
Steph <superpowers04@cybergal.com>
2025-09-14 22:42:27 -0400

    Only check CANCEL_DAMAGE if damage event actually tries to cancel it

--- common/src/main/java/org/figuramc/figura/avatar/Avatar.java
+++ common/src/main/java/org/figuramc/figura/avatar/Avatar.java
@@ -503,7 +503,7 @@ public class Avatar {
 
 
 	public boolean damageEvent(String sourceType, EntityAPI<?> sourceCause, EntityAPI<?> sourceDirect, FiguraVec3 sourcePosition) {
-		if (loaded) run("DAMAGE", tick, sourceType, sourceCause, sourceDirect, sourcePosition);
+		return isCancelled(loaded ? run("DAMAGE", tick, sourceType, sourceCause, sourceDirect, sourcePosition) : null);
 	}
 	public void attackEvent(String sourceType, EntityAPI<?> loser, FiguraVec3 sourcePosition) {
 		if (loaded) run("ATTACK", tick, sourceType, loser, sourcePosition);
