16da99a57e87c925e480fe0d9db687b3d790afc2
tivvo <tiveshp147@gmail.com>
2025-09-29 13:36:11 +0200

    WUTHERING HEIGHTS

--- common/src/main/java/org/figuramc/figura/lua/api/ClientAPI.java
+++ common/src/main/java/org/figuramc/figura/lua/api/ClientAPI.java
@@ -240,7 +240,7 @@ public class ClientAPI {
 	@LuaMethodDoc("client.get_window_size")
 	public static FiguraVec2 getWindowSize() {
 		Window window = Minecraft.getInstance().getWindow();
-		return FiguraVec2.of(window.getWidth(), window.getHeight());
+		return FiguraVec2.of(window.getScreenWidth(), window.getScreenHeight());
 	}
 
 	@LuaWhitelist
@@ -259,10 +259,11 @@ public class ClientAPI {
 	@LuaMethodDoc("client.get_mouse_pos")
 	public static FiguraVec2 getMousePos() {
 		MouseHandler mouse = Minecraft.getInstance().mouseHandler;
-		FloatBuffer xScale = BufferUtils.createFloatBuffer(1);
-		FloatBuffer yScale = BufferUtils.createFloatBuffer(1);
-		GLFW.glfwGetWindowContentScale( (long) Minecraft.getInstance().getWindow().getWindow(), xScale, yScale);
-		return FiguraVec2.of(mouse.xpos() * xScale.get(0), mouse.ypos() * yScale.get(0));
+		//FloatBuffer xScale = BufferUtils.createFloatBuffer(1);
+		//FloatBuffer yScale = BufferUtils.createFloatBuffer(1);
+		//GLFW.glfwGetWindowContentScale( (long) Minecraft.getInstance().getWindow().getWindow(), xScale, yScale);
+		//return FiguraVec2.of(mouse.xpos() * xScale.get(0), mouse.ypos() * yScale.get(0));
+		return FiguraVec2.of(mouse.xpos(), mouse.ypos());
 	}
 
 	@LuaWhitelist
