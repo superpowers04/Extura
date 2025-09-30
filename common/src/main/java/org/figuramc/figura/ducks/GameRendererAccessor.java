ff171190954e4fc584431a01c7f27e7c423899ad
Steph <superpowers04@cybergal.com>
2025-09-29 22:07:51 -0400

    might be small brained

--- common/src/main/java/org/figuramc/figura/ducks/GameRendererAccessor.java
+++ common/src/main/java/org/figuramc/figura/ducks/GameRendererAccessor.java
@@ -1,8 +1,11 @@
 package org.figuramc.figura.ducks;
 
+import org.joml.Matrix4f;
+
 import net.minecraft.client.Camera;
 
 public interface GameRendererAccessor {
+    Matrix4f figura$getBobbingMatrix();
 
     double figura$getFov(Camera camera, float tickDelta, boolean changingFov);
 }
