694565e7f90c77a82a5ff50730db5525733fb9e6
Steph <38338700+superpowers04@users.noreply.github.com>
2026-04-14 23:34:39 -0400

    Fix crash when avatar doesn't have version

--- common/src/main/java/org/figuramc/figura/gui/widgets/permissions/PlayerStatusWidget.java
+++ common/src/main/java/org/figuramc/figura/gui/widgets/permissions/PlayerStatusWidget.java
@@ -78,7 +78,7 @@ public class PlayerStatusWidget extends StatusWidget {
         // script render
         render = avatar.scriptError ? 1 : avatar.luaRuntime == null ? 0 : avatar.render.getTotal() >= avatar.permissions.get(Permissions.RENDER_INST) * 0.75 || avatar.worldRender.getTotal() >= avatar.permissions.get(Permissions.WORLD_RENDER_INST) * 0.75 ? 2 : 3;
     	backend = avatar.scriptError ? 1 : FSB.instance().isPlayerConnected(owner) ? 4 : 3;
-    	version = avatar.version.compareTo(FiguraMod.VERSION) == 0 ? 3 : 2;
+    	version = (avatar.version != null && avatar.version.compareTo(FiguraMod.VERSION) == 0) ? 3 : 2;
      }
 
     @Override
